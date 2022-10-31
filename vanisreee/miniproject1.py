8de25b5b0aeb0d4facc5d3dd85d4ea278eecb68c
vanisreebovilla <vanisreebovilla123@gmail.com>
2022-10-31 18:25:16 +0530

    this is  my code

---
+++ vanisreee/miniproject1.py
@@ -0,0 +1,9 @@
+name=input("Name:")
+clss=int(input("class"))
+section=input("section:")
+school=input("school")
+classteacher=input("class teacher name:")
+date=int(input("date"))
+reason=input("reason:")
+newstring=f"Dear {classteacher},i am {name}.Studying {clss} {section} {school}.I suffering from {reason}.Iwant leave from {date}.Thank you"
+print(newstring)
